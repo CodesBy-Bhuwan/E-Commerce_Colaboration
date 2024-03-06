22be6a32db0895620e72cdddc5cc39d8723d6d16
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-03-06 21:57:34 +0545

    initiating backend section

---
+++ ecommercebackendsystem/src/test/java/com/ecommerce/ecommercebackendsystem/EcommercebackendsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommercebackendsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercebackendsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
